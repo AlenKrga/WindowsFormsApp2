050628258b64c8058f28667c969787d3a0665a8a
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-05-08 22:03:13 +0100

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Button 1 clicked!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Button 2 clicked!";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Button 3 clicked!";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label3.Text = "Radio Button 1 is selected!";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label3.Text = "Radio Button 2 is selected!";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            label3.Text = "Radio Button 3 is selected!";
+        }
+    }
+}
